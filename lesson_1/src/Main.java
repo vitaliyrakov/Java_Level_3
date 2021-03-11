e8e946d63e9e3a2a5706ee2fcb3e241a75a2e7c7
vitaliyR <74015913+vitaliyrakov@users.noreply.github.com>
2021-03-11 14:03:35 +0300

    Update Main.java

--- lesson_1/src/Main.java
+++ lesson_1/src/Main.java
@@ -1,14 +1,64 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-//        1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
-//        2. Написать метод, который преобразует массив в ArrayList;
-//        3. Большая задача:
-//        a. Есть классы Fruit -> Apple, Orange;(больше фруктов не надо)
-//        b. Класс Box в который можно складывать фрукты, коробки условно сортируются по типу фрукта, поэтому в одну коробку нельзя сложить и яблоки, и апельсины;
-//        c. Для хранения фруктов внутри коробки можете использовать ArrayList;
-//        d. Сделать метод getWeight() который высчитывает вес коробки, зная количество фруктов и вес одного фрукта(вес яблока - 1.0f, апельсина - 1.5f, не важно в каких это единицах);
-//        e. Внутри класса коробка сделать метод compare, который позволяет сравнить текущую коробку с той, которую подадут в compare в качестве параметра, true - если их веса равны, false в противном случае(коробки с яблоками мы можем сравнивать с коробками с апельсинами);
-//        f. Написать метод, который позволяет пересыпать фрукты из текущей коробки в другую коробку(помним про сортировку фруктов, нельзя яблоки высыпать в коробку с апельсинами), соответственно в текущей коробке фруктов не остается, а в другую перекидываются объекты, которые были в этой коробке;
-//        g. Не забываем про метод добавления фрукта в коробку.
+
+        String[] arr = new String[]{"Зима", "Весна", "Лето"};
+        System.out.println(Arrays.toString(arr));
+        chgElemArray(arr, 0, 2);
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println(Arrays.toString(arr));
+        List<String> list = transArrToList(arr);
+        System.out.println(list.toString());
+
+        Box<Apple> appleBox = new Box<>(new Apple(), new Apple(1.1f), new Apple(2.5f));
+        Box<Orange> orangeBox = new Box<>(new Orange(), new Orange(2.1f), new Orange(2.5f));
+        System.out.println(appleBox.getQty() + "шт. " + appleBox.getWeight());
+        System.out.println(orangeBox.getQty() + "тш. " + orangeBox.getWeight());
+
+        appleBox.add(new Apple(2.0f));
+        appleBox.add(new Apple(), new Apple());
+        System.out.println(appleBox.getQty() + "шт.");
+        Box<Apple> newAppleBox = new Box();
+        newAppleBox.fill(appleBox);
+        System.out.println(appleBox.getQty() + "шт.");
+        System.out.println("Коробка с яблоками новая " + newAppleBox.getQty() + "шт." + orangeBox.getWeight());
+
+        orangeBox.add(new Orange(2.1f));
+        orangeBox.remove(new Orange());
+        System.out.println("Коробка с апельсинами " + orangeBox.getQty() + "шт. " + orangeBox.getWeight());
+
+        if (newAppleBox.compare(orangeBox)) {
+            System.out.println("Веса равны");
+        } else {
+            System.out.println("Веса отличны");
+        }
+
     }
+
+
+    // 1. Написать метод, который меняет два элемента массива местами.(массив может быть любого ссылочного типа);
+    public static <T> void chgElemArray(T[] arr, int el1, int el2) {
+        if (el1 < 0 || el2 < 0 || el1 >= arr.length || el2 >= arr.length)
+            throw new ArrayIndexOutOfBoundsException("Указанные элемены больше размерности массива");
+
+        T tmp = arr[el1];
+        arr[el1] = arr[el2];
+        arr[el2] = tmp;
+    }
+
+    // 2. Написать метод, который преобразует массив в ArrayList;
+    public static <T> List<T> transArrToList(T[] arr) {
+        if (arr == null) return new ArrayList<>();
+
+        List<T> arrList = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            arrList.add(arr[i]);
+        }
+        return arrList;
+    }
+
 }
